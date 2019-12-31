194c332b94b8937a66eb3b8971edb3161a5bdbe5
hojinh89 <hjok2002@naver.com>
2020-01-01 07:30:21 +0900

    11

--- src/hello.java
+++ src/hello.java
@@ -1,9 +1,18 @@
 
+import java.io.FileOutputStream;
+import java.io.IOException;
+
 public class hello {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("nyanya");
+	public static void main(String[] args) throws IOException {
+		
+		FileOutputStream output = new FileOutputStream("c:/out.txt");
+		for(int i=1; i<11; i++) {
+			String data = i + "번째 줄입니다.\r\n";
+			output.write(data.getBytes());
+		}
+		
+		output.close();
 	}
 
 }
